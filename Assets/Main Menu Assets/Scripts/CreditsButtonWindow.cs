bef9d85c5d07509e6cffc62900f42b4c3ad4f641
serynabatov <serynabatov@yandex.ru>
2021-09-29 19:39:42 +0300

    Changed animation

--- Assets/Main Menu Assets/Scripts/CreditsButtonWindow.cs
+++ Assets/Main Menu Assets/Scripts/CreditsButtonWindow.cs
@@ -8,12 +8,16 @@ public class CreditsButtonWindow : MonoBehaviour
     public void OnEnable()
     {
 
-        box.localPosition = new Vector2(0, -Screen.height);
-        box.LeanMoveLocalY(0, 0.5f).setEaseInOutExpo().delay = 0.1f;
+        //box.localPosition = new Vector2(0, -Screen.height);
+        box.localPosition = new Vector2(-Screen.width + 789, 0);
+        //box.LeanMoveLocalY(0, 0.5f).setEaseInOutExpo().delay = 0.1f;
+        box.LeanMoveLocalX(18, 0.5f).setEaseInOutExpo().delay = 0.1f;
+        //box.LeanScale(Vector2.one, 0.8f);
     }
     public void CloseDialog()
     {
-        box.LeanMoveLocalY(-Screen.height, 1f).setEaseInOutExpo().setOnComplete(Complete);
+        //box.LeanMoveLocalY(-Screen.height, 1f).setEaseInOutExpo().setOnComplete(Complete);
+        box.LeanMoveLocalX(-Screen.width + 789, 1f).setEaseInOutExpo().setOnComplete(Complete);
     }
 
     public void Complete()
