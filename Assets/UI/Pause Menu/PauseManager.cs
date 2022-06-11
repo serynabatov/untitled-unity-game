e4586b9dc9b178f751055de7071f0b8aa97bfa4a
MexaBubuLiK <79213259+MexaBubuLiK@users.noreply.github.com>
2022-06-11 15:43:43 +0300

    Настроил меню Options и вкладку Graphics и звук салйдер Music Slider также работает и сохраняется

--- Assets/UI/Pause Menu/PauseManager.cs
+++ Assets/UI/Pause Menu/PauseManager.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
 {
     public static bool paused;
+    List<int> widths = new List<int>() { 1920, 1280, 960 };
+    List<int> heights = new List<int>() { 1080, 800, 540 };
     [SerializeField] GameObject pauseBackground;
+    [Header("Graphics")]
+    [SerializeField] Toggle fullscreenToogle;
+    [SerializeField] TMP_Dropdown resolutionDropdown;
+    [Header("Sounds")]
+    [SerializeField] Slider musicVolumeSlider;
     private GameObject optionsMenu;
 
     private static PauseManager instance;
@@ -29,6 +37,15 @@ public class PauseManager : MonoBehaviour
     private void Start()
     {
         optionsMenu = pauseBackground.transform.GetChild(1).gameObject;
+        //! Разрешение экрана дефолтное изменённое запускаем и фуллскрин или в окне
+        SetResolution(PlayerPrefs.GetInt("defaultIndexResolution", 0));
+        SetFullscreen(PlayerPrefs.GetInt("fullscreenStatus", 1) == 1 ? true : false);
+
+        //* Громкость музыки настройка
+        SetMusicVolume(PlayerPrefs.GetFloat("MusicVolume", 1.0f));
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
+
+
     }
 
     private void Update()
@@ -79,5 +96,37 @@ public class PauseManager : MonoBehaviour
     public void ExitToMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    /// <summary>
+    //  *Sets resolution
+    /// </summary>
+    /// <param name="index"></param>
+    public void SetResolution(int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+        PlayerPrefs.SetInt("defaultIndexResolution", index);
+
+        resolutionDropdown.value = index;
+
+    }
+    /// <summary>
+    /// *Sets fullscreen status
+    /// </summary>
+    /// <param name="_fullscreen"></param>
+    public void SetFullscreen(bool _fullscreen)
+    {
+        Screen.fullScreen = _fullscreen;
+        fullscreenToogle.isOn = _fullscreen;
+        PlayerPrefs.SetInt("fullscreenStatus", _fullscreen == true ? 1 : 0);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("MusicVolume", volume);
     }
 }
