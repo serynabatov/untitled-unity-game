26acf7d9fefecb523bb10b274b8651c076093f4e
MexaBubuLiK <79213259+MexaBubuLiK@users.noreply.github.com>
2022-11-26 14:25:52 +0300

    Fixed path run.py

--- DevScripts/run.py
+++ DevScripts/run.py
@@ -20,6 +20,4 @@ if __name__ == "__main__":
     with open("folder_names.json") as file:
         data = json.load(file)
 
-        # for folder_name in data:
-        #     download_files.download_folder(folder_name)
-        go_through_subfolders(data, ".")
+        go_through_subfolders(data, "./Assets/Sprites")
