195d5eb5f4ec03471b26f07c6ba1efe9bda24fbd
Genkir <vadim.coy.98@mail.ru>
2023-02-22 17:24:11 +0300

    added water riddle

---
+++ Assets/Water Riddle/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject clearButton;
+    public GameObject gameOverNote;
+    public GameObject restartButton;
+    public GameObject stateText;
+    public GameObject startButton;
+    public GameObject clearText;
+    public GameObject flow;
+
+    public bool gameRunning;
+    public bool gameOver;
+    public bool gameClear;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            gameOverNote.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }
+        if (gameClear)
+        {
+            clearText.gameObject.SetActive(true);
+            clearButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void GameRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //edge.gameObject.SetActive(true);
+    }
+    public void GameStart()
+    {
+        gameRunning = true;
+        stateText.gameObject.SetActive(false);
+        startButton.gameObject.SetActive(false);
+    }
+
+    public void LoadScene()
+    {
+        int index;
+        index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Water Riddle/Scripts/PipeController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PipeController : MonoBehaviour
+{
+    private Animator animator;
+
+    private AudioSource sound;
+
+    private GameManager gameManager;
+
+    private Collider2D collide;
+
+    private Camera cam;
+
+    public int AngleState;
+    public int PipeState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        collide = gameObject.GetComponent<CircleCollider2D>();
+        gameManager = GameObject.Find("GameManagers").GetComponent<GameManager>();
+        animator = gameObject.GetComponent<Animator>();
+        sound = gameObject.GetComponent<AudioSource>();
+        if (gameObject.CompareTag("Turn"))
+            AngleAnimationsCheck();
+        if (gameObject.CompareTag("Pipe"))
+            PipeAnimationsCheck();
+    }
+
+    void Update()
+    {
+        Mouse mouse = Mouse.current;
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            Vector3 mousePosition = mouse.position.ReadValue();
+            if (collide.OverlapPoint(cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 1))))
+            {
+                sound.Play();
+                if (!gameManager.gameRunning)
+                {
+                    if (gameObject.CompareTag("Turn"))
+                    {
+                        AngleState++;
+                        AngleState = AngleState % 4;
+                        AngleAnimationsCheck();
+                    }
+                    if (gameObject.CompareTag("Pipe"))
+                    {
+                        PipeState++;
+                        PipeState = PipeState % 2;
+                        PipeAnimationsCheck();
+                    }
+                }
+                Debug.Log(mousePosition);
+                Debug.Log("Hit");
+            }
+        }
+    }
+
+    private void AngleAnimationsCheck()
+    {
+        if (gameObject.CompareTag("Turn"))
+        {
+            switch (AngleState)
+            {
+                case 0: // Left - Down
+                    {
+                        animator.Play("Left - Down");
+                        break;
+                    }
+                case 1: // Left - Up
+                    {
+                        animator.Play("Left - Up");
+                        break;
+                    }
+                case 2: // Up - Right
+                    {
+                        animator.Play("Right - Up");
+                        break;
+                    }
+                case 3: // Right - Down
+                    {
+                        animator.Play("Right - Down");
+                        break;
+                    }
+            }
+        }
+    }
+
+    private void PipeAnimationsCheck()
+    {
+        if (gameObject.CompareTag("Pipe"))
+        {
+            switch (PipeState)
+            {
+                case 0: // Left - Right
+                    animator.Play("Horizontal");
+                    break;
+                case 1: // Up - Down
+                    animator.Play("Vertical");
+                    break;
+            }
+        }
+    }
+}
