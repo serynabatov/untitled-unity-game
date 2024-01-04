a7b0afbd24fb775c499997b177fa576b029eba10
m.musaev <m.musaev@corp.nekki.ru>
2024-01-04 13:08:25 +0300

    animation location name

--- Assets/Gameplay Assets/Scripts/LocationCheck.cs
+++ Assets/Gameplay Assets/Scripts/LocationCheck.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class LocationCheck : MonoBehaviour
 {
     [SerializeField]
-    private Localizator localizator;
+    private GameObject location;
     [SerializeField]
     private string iD;
 
     public void ChangeLocationName()
     {
+        Localizator localizator = location.gameObject.GetComponent<Localizator>();
+        Animator anim = location.gameObject.GetComponent<Animator>();
         localizator.iD = iD;
+        anim.SetTrigger("RevealLocation");
     }
 }
