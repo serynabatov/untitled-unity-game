350b0dfdd5e41d216ee3467edad10463602587cd
Genkir <vadim.coy.98@mail.ru>
2024-01-08 13:10:54 +0300

    Fixes with waterwell

---
+++ Assets/Gameplay Assets/Scripts/Water Fall animations/WellWater.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellWater : MonoBehaviour, IAnimationPlayable
+{
+    private Animator anim;
+    private int status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeginPlaying();
+    }
+
+    public void BeginPlaying()
+    {
+        anim = GetComponent<Animator>();
+        status = PlayerPrefs.GetInt("Water level status");
+        print(status);
+        if (status == 1)
+        {
+            anim.Play("Well");
+        }
+        else
+        {
+            anim.Play("WellDefault");
+        }
+    }
+}
